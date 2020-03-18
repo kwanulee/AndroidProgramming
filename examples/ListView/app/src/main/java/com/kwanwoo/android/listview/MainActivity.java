3e46c927abf1ba99dfb59ce441d26d9ca37c3279
kwanulee <kwlee@hansung.ac.kr>
2020-03-18 23:45:40 +0900

    build with api 29, griddle 3.6.1, androidX

--- examples/ListView/app/src/main/java/com/kwanwoo/android/listview/MainActivity.java
+++ examples/ListView/app/src/main/java/com/kwanwoo/android/listview/MainActivity.java
@@ -2,7 +2,7 @@ package com.kwanwoo.android.listview;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.ArrayAdapter;
