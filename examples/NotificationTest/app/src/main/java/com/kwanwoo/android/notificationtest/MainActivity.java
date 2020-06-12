305dfe854b5ace498df86aa48beef05fe6d8f572
kwanulee <kwlee@hansung.ac.kr>
2020-06-13 07:26:54 +0900

    build with android, api29

--- examples/NotificationTest/app/src/main/java/com/kwanwoo/android/notificationtest/MainActivity.java
+++ examples/NotificationTest/app/src/main/java/com/kwanwoo/android/notificationtest/MainActivity.java
@@ -7,19 +7,19 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 
 import android.graphics.BitmapFactory;
 
 import android.graphics.Color;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.os.Build;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
-
-import static com.kwanwoo.android.notificationtest.R.mipmap.ic_launcher;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NOTI_ID = 100;
@@ -38,57 +38,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void initChannel() {
-        if (android.os.Build.VERSION.SDK_INT >=26) {
-            // The id of the channel.
-            String id = "default";
+    final String CHANNEL_ID="my_channel_01";
 
-            // The user-visible name of the channel.
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
-
-            // The user-visible description of the channel.
             String description = getString(R.string.channel_description);
-
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
 
             // Configure the notification channel.
-            mChannel.setDescription(description);
-            mChannel.enableLights(true);
-            mChannel.setLightColor(Color.BLUE);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
 
-            NotificationManager mNotificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            mNotificationManager.createNotificationChannel(mChannel);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
-
+    
     public void NotificationSomethings() {
 
-        initChannel();
+        createNotificationChannel();
 
         Intent notificationIntent = new Intent(this, NotificationDetail.class);
-        notificationIntent.putExtra("notificationId", 9999); //전달할 값
+        notificationIntent.putExtra("notificationId", NOTI_ID); //전달할 값
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "default");
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID);
         notification.setContentTitle(getString(R.string.notification_title))
                 .setContentText(getString(R.string.notification_text))
                 .setSmallIcon(R.drawable.ic_access_alarm_black_24dp)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setWhen(System.currentTimeMillis())
-                .setPriority(Notification.PRIORITY_HIGH)
-                .setDefaults(Notification.DEFAULT_ALL);
+                .setContentIntent(contentIntent);
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(NOTI_ID, notification.build());
-
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTI_ID, notification.build());
     }
 
 
--- examples/NotificationTest/app/src/main/java/com/kwanwoo/android/notificationtest/NotificationDetail.java
+++ examples/NotificationTest/app/src/main/java/com/kwanwoo/android/notificationtest/NotificationDetail.java
@@ -2,7 +2,7 @@ package com.kwanwoo.android.notificationtest;
 
 import android.app.NotificationManager;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
